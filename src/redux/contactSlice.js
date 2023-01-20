219eaa1e133eabffb360cc4a918b6462ee55f3ad
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-01-20 20:08:38 +0200

    add redux

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const contactSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    items: [],
+    filter: '',
+  },
+  reducers: {
+    setStatusFilter(state, action) {
+        state.filter = action.payload;
+    },
+    addContacts(state, action) {
+      state.items.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { setStatusFilter, addContact, deleteContact } = contactSlice.actions;
+
