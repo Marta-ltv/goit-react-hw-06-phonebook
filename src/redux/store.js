846b23775e741a3b475a14476f5a188104574e36
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-01-21 13:57:11 +0200

    add localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactSlice } from './contactSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const contactReducer = persistReducer(persistConfig, contactSlice.reducer);
 
 export const store = configureStore({
-    reducer: {
-      contacts: contactSlice.reducer
+  reducer: {
+    contacts: contactReducer,
   },
-})
+    middleware(getDefaultMiddleware) {
+      return getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      });
+    },
+});
+
+export const persistor = persistStore(store)
